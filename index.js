fa174acfb0d6e137245c899eed812ea824e08f0f
Austin Baik <baik.austin@gmail.com>
2021-12-19 18:50:25 -0800

    commit tree a2dd6adc890f5d2ed7ba33b940546fb6f9b4c2e3

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+let dist;
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(dist){
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  if (dist <= 400){
+    return ('This one is on me!');
+  }
+    else if (dist > 2500){
+      return ('No can do.');
+  }
+    else if (dist > 2000){
+      return ('I will gladly take your thirty bucks.');
+    }
+  }
+
+
+let city = "NYC";
+let name;
+
+  function ternaryCheckCity(name){
+   return (name === city ? 'Ok, sounds good.' : 'No go.');
+  }
+  
+
+let tip; 
+  function switchOnCharmFromTip(tip){
+    switch(tip){
+      case 'generous':
+      return  'Thank you so much.';
+      break;
+      case 'not as generous':
+      return 'Thank you.';
+      break; 
+      case 'thanks for everything':
+      return 'Bye.';
+    }
+  }
